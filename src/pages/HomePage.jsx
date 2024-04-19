992fcf68124d9c576e7a36cee36caa432410b9cc
Hemyulin <daniel.budiansky@protonmail.com>
2024-04-19 19:47:01 +0200

    testing ground

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,16 @@
+import "./HomePage.css";
+
+export const HomePage = () => {
+  return (
+    <div className="test">
+      <div className="one">
+        <div>LOL</div>
+        <div>NO</div>
+      </div>
+      <div className="two">
+        <div>sdf</div>
+        <div>LOL</div>
+      </div>
+    </div>
+  );
+};
