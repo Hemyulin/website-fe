4e2bf997b908a7b140248c671a92e78ebc385b53
Hemyulin <daniel.budiansky@protonmail.com>
2024-04-20 20:54:02 +0200

    10:39

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import "./app.scss";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <section>
+        <a href="">Hello World!</a>
+      </section>
+      <section>Parallax</section>
+      <section>Services</section>
+      <section>Parallax</section>
+      <section>Portfolio1</section>
+      <section>Portfolio2</section>
+      <section>Portfolio3</section>
+      <section>Contacts</section>
+    </div>
+  );
 }
 
 export default App;
