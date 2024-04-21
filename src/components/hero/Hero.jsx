9b9235bc05c69b9bfc8f92393456897a393f8d82
Hemyulin <daniel.budiansky@protonmail.com>
2024-04-21 22:42:36 +0200

    01:06:54 hero done

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -1,22 +1,79 @@
+import { motion } from "framer-motion";
 import "./Hero.scss";
+
+const textVeriants = {
+  initial: {
+    x: -500,
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 1,
+      staggerChildren: 0.1,
+    },
+  },
+  scrollButton: {
+    opacity: 0,
+    y: 10,
+    transition: {
+      duration: 2,
+      repeat: Infinity,
+    },
+  },
+};
+
+const sliderVeriants = {
+  initial: {
+    x: 0,
+  },
+  animate: {
+    x: "-220%",
+    transition: {
+      repeat: Infinity,
+      repeatType: "mirror",
+      duration: 20,
+    },
+  },
+};
 
 export const Hero = () => {
   return (
     <div className="hero">
       <div className="wrapper">
-        <div className="textContainer">
-          <h2>DANIEL BUDIANSKY</h2>
-          <h1>Full stack web developer</h1>
-          <div className="buttons">
-            <button>See the latest works</button>
-            <button>Contact me</button>
-          </div>
-          <img src="/scroll.png" alt="scroll down icon" />
-        </div>
+        <motion.div
+          className="textContainer"
+          variants={textVeriants}
+          initial="initial"
+          animate="animate"
+        >
+          <motion.h2 variants={textVeriants}>DANIEL BUDIANSKY</motion.h2>
+          <motion.h1 variants={textVeriants}>
+            Full stack web developer
+          </motion.h1>
+          <motion.div className="buttons" variants={textVeriants}>
+            <motion.button variants={textVeriants}>
+              See the latest works
+            </motion.button>
+            <motion.button variants={textVeriants}>Contact me</motion.button>
+          </motion.div>
+          <motion.img
+            src="/scroll.png"
+            alt="scroll down icon"
+            variants={textVeriants}
+            animate="scrollButton"
+          />
+        </motion.div>
       </div>
-      <div className="slidingTextContainer">
-        Writer content creater polyglot
-      </div>
+      <motion.div
+        className="slidingTextContainer"
+        variants={sliderVeriants}
+        initial="initial"
+        animate="animate"
+      >
+        React TypeScript Express Node.JS MongoDB Flutter
+      </motion.div>
       <div className="imageContainer">
         <img src="/hero.png" alt="picture of Daniel Budiansky" />
       </div>
