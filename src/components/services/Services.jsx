da6ed1dceebffcf26ffd187438f3fdcfe2a8d099
Hemyulin <daniel.budiansky@protonmail.com>
2024-04-25 14:38:46 +0200

    2:34:54

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -2,7 +2,7 @@ import { useRef } from "react";
 import "./Services.scss";
 import { motion, useInView } from "framer-motion";
 
-const vatiants = {
+const variants = {
   initial: {
     x: -500,
     y: 100,
@@ -21,52 +21,52 @@ const vatiants = {
 
 export const Services = () => {
   const ref = useRef();
+
   const isInView = useInView(ref, { margin: "-100px" });
 
   return (
     <motion.div
       className="services"
-      variants={vatiants}
+      variants={variants}
       initial="initial"
-      //   animate="animate"
-      //   whileInView="animate"
+      // animate="animate"
+      // whileInView="animate"
       ref={ref}
-      animate={isInView && "animate"}
+      animate={"animate"}
     >
-      <motion.div className="textContainer" variants={vatiants}>
+      <motion.div className="textContainer" variants={variants}>
         <p>
-          I focus on helping your brand grow <br /> and move forward
+          I focus on helping your brand grow
+          <br /> and move forward
         </p>
         <hr />
       </motion.div>
-
-      <motion.div className="titleContainer" variants={vatiants}>
+      <motion.div className="titleContainer" variants={variants}>
         <div className="title">
-          <img src="/people.webp" alt="a bunch of people in a room" />
+          <img src="/people.webp" alt="" />
           <h1>
-            <motion.b whileHover={{ color: "orange" }}>Unique </motion.b>
-            Ideas
+            <motion.b whileHover={{ color: "orange" }}>Unique</motion.b> Ideas
           </h1>
         </div>
         <div className="title">
           <h1>
-            <motion.b whileHover={{ color: "orange" }}>For Your </motion.b>
+            <motion.b whileHover={{ color: "orange" }}>For Your</motion.b>{" "}
             Business.
           </h1>
           <button>WHAT WE DO?</button>
         </div>
       </motion.div>
-
-      <motion.div className="listContainer" variants={vatiants}>
+      <motion.div className="listContainer" variants={variants}>
         <motion.div
           className="box"
           whileHover={{ background: "lightgray", color: "black" }}
         >
           <h2>Branding</h2>
           <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+            libero enim nisi aliquam consectetur expedita magni eius ex corrupti
+            animi! Ad nam pariatur assumenda quae mollitia libero repellat
+            explicabo maiores?
           </p>
           <button>Go</button>
         </motion.div>
@@ -76,9 +76,10 @@ export const Services = () => {
         >
           <h2>Branding</h2>
           <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+            libero enim nisi aliquam consectetur expedita magni eius ex corrupti
+            animi! Ad nam pariatur assumenda quae mollitia libero repellat
+            explicabo maiores?
           </p>
           <button>Go</button>
         </motion.div>
@@ -88,9 +89,10 @@ export const Services = () => {
         >
           <h2>Branding</h2>
           <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+            libero enim nisi aliquam consectetur expedita magni eius ex corrupti
+            animi! Ad nam pariatur assumenda quae mollitia libero repellat
+            explicabo maiores?
           </p>
           <button>Go</button>
         </motion.div>
@@ -100,9 +102,10 @@ export const Services = () => {
         >
           <h2>Branding</h2>
           <p>
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
+            libero enim nisi aliquam consectetur expedita magni eius ex corrupti
+            animi! Ad nam pariatur assumenda quae mollitia libero repellat
+            explicabo maiores?
           </p>
           <button>Go</button>
         </motion.div>
